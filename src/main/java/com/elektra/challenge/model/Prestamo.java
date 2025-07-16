a4036fbfe5ee462d670d9033173f7551c75fde4a
Steve Sanchez <1steven.nathan.sanchez@gmail.com>
2025-07-16 11:37:32 -0600

    feat: agregar Clase Prestamo

---
+++ src/main/java/com/elektra/challenge/model/Prestamo.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Prestamo {
+    private String id;
+    private double monto;
+    private String clienteId;
+    private LocalDate fecha;
+    private EstadoPrestamo estado;
+}
